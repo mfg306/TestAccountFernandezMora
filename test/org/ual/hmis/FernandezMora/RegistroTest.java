dfe73dae18931a9d85832fac9b46c0492e8ac987
Marta Fernández <mfg306@inlumine.ual.es>
2021-04-30 11:35:38 +0200

    Comentando drivers

--- test/org/ual/hmis/FernandezMora/RegistroTest.java
+++ test/org/ual/hmis/FernandezMora/RegistroTest.java
@@ -63,7 +63,7 @@ public class RegistroTest {
 			// Descargar geckodriver de https://github.com/mozilla/geckodriver/releases
 			// Descomprimir el archivo geckodriver.exe en la carpeta drivers
 
-			System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+//			System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
 			FirefoxOptions firefoxOptions = new FirefoxOptions();
 			if (headless)
 				firefoxOptions.setHeadless(headless);
@@ -75,7 +75,7 @@ public class RegistroTest {
 			// Descargar Chromedriver de https://chromedriver.chromium.org/downloads
 			// Descomprimir el archivo chromedriver.exe en la carpeta drivers
 
-			System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+//			System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 			ChromeOptions chromeOptions = new ChromeOptions();
 			if (headless)
 				chromeOptions.setHeadless(headless);
